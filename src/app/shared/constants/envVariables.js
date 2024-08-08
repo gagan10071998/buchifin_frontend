c59b129692357cc417694892e63502b8209926ec
AFK-NOCAP <gagan@odrways.com>
2024-08-08 23:19:08 +0530

    api url for prod added

--- src/app/shared/constants/envVariables.js
+++ src/app/shared/constants/envVariables.js
@@ -1,6 +1,6 @@
 // export const API_URL = 'https://api.gov.sama.live';
 // export const API_URL = 'http://localhost:9072';
-export const API_URL = 'http://52.66.116.81';
+export const API_URL = 'http://13.232.126.175';
 export const API_ENDPOINT_PREFIX = 'api';
 export const API_VERSION = 'v1';
 export const API_BASE_URL = `${API_URL}/${API_ENDPOINT_PREFIX}/${API_VERSION}`;
